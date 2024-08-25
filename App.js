496338b237480afed914e9dc2d4e0010b695d842
anhtrieuthitam <trieuthitamanha1@gmail.com>
2024-08-25 10:50:55 +0700

    Initial commit for Advanced Mobile Programming Exercises Week 1

---
+++ App.js
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const profile = {
+  name: 'Trieu Thi Tam Anh',
+  studentId: '21110128',
+  email: 'trieuthitamanha1@gmail.com',
+  github: 'https://github.com/fwishy121',
+  summary: "I am a Fullstack Developer",
+}
+
+let timerId = null;
+
+function HomeScreen({ navigation }) {
+  useFocusEffect(() => { 
+    timerId = setTimeout(() => {
+      navigation.navigate('Details', { profile });
+    }, 5000);
+
+    return () => { clearTimeout(timerId) }
+  });
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome to my journey!</Text>
+      <Text>Now, let's see what my information is</Text>
+      <Text>Waiting for 5s</Text>
+    </View>
+  );
+}
+
+function DetailsScreen({ route, navigation }) {
+  const { profile } = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.profileContainer}>
+        <Image
+          source={require('./assets/pig_cry.jpg')}
+          style={styles.profilePicture}
+        />
+        <Text style={styles.profileName}>{profile.name}</Text>
+        <Text style={styles.profileInfo}>Student ID: {profile.studentId}</Text>
+        <Text style={styles.profileInfo}>Email: {profile.email}</Text>
+        <Text style={styles.profileInfo} onPress={() => Linking.openURL(profile.github)}>
+          GitHub: {profile.github}
+        </Text>
+        <Text style={styles.profileSummary}>{profile.summary}</Text>
+      </View>
+
+      <Button title="Back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    backgroundColor: '#f7f7f7',
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+  profileName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  profileInfo: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 5,
+  },
+  profileSummary: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 10,
+  },
+});
+
+export default App;
